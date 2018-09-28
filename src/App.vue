b3690f628c4a5b31fb0fb8d1656caea25a8c1dea
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-09-28 13:29:56 +0300

    Add start, reset, submit buttons.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div id="app">
-    <button @click="generateRandomNumber">Start</button></br>
-    <div id="star-section" v-for="star in stars">
-      <font-awesome-icon icon="star" />
+    <div id="start-button" v-if="step === 0">
+      <button @click="generateRandomNumber">START</button>
     </div>
-    <div id="answer-section">
-      <strong>ANS:</strong> 
-    </div>
-    <div id="select-section">
-      <ul class="select-inner-section" v-for="num in 9">
-        <li class="select-digit">{{num}}</li>
-      </ul>
+    <div v-if="step === 1">
+      <div id="star-section" v-for="star in stars">
+        <font-awesome-icon icon="star" />
+      </div>
+      <div id="answer-section">
+        <button @click="reset">Reset</button>
+        <button class="answer-section"><strong>ANS: {{ answer }}</strong></button>
+        <button @click="generateRandomNumber">Submit</button>
+      </div>
+      <div id="select-section">
+        <ul class="select-inner-section" v-for="num in 9">
+          <li class="select-digit" @click="generateAnswer(num)">{{num}}</li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -20,12 +26,22 @@ export default {
   name: 'app',
   data () {
     return {
-      stars: 0
+      stars: 0,
+      answer: '',
+      step: 0
     }
   },
   methods: {
-    generateRandomNumber () {
-      this.stars = Math.floor(Math.random() * (10 - 1 +1) + 1 )
+    generateRandomNumber() {
+      // Math.floor(Math.random() * (max -min +1)) + min;
+      this.stars = Math.floor(Math.random() * (10 - 1 +1) + 1 );
+      this.step = 1;
+    },
+    generateAnswer(num) {
+      this.answer = this.answer + num;
+    },
+    reset() {
+      this.answer = '';
     }
   }
 }
@@ -38,7 +54,11 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 200px;
+  margin: 200px 30%;
+  background-color: #E6E6FA;
+  padding: 25px;
+  border-radius: 3px;
+  min-height: 500px;
 }
 
 h1, h2 {
@@ -64,12 +84,23 @@ a {
   padding: 40px 0;
 }
 
-#answer-section {
-  margin: 40px 0;
-  background-color: lightblue;
-  width: 5%;
-  margin: auto;
+#start-button {
+  position: absolute;
+  top: 35%;
+  left: 47%;
+}
+
+#start-button button {
   padding: 15px;
+  font-size: 2rem;
+  font-weight: bold;
+  border-radius: 5px;
+}
+
+.answer-section {
+  background-color: lightblue;
+  padding: 15px;
+  margin: 0 10px;
 }
 
 #select-section {
