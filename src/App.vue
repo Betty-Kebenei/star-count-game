099bf4cbdeffe9c3d97d4d69bcd5f8dba4f3d3bd
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-09-28 11:22:05 +0300

    Add the initial project setup

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    {{msg}} <br />
+    <ul class="select-section" v-for="num in 9">
+      <li class="select-digit">{{num}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+.select-section{
+  display: inline;
+  
+}
+
+.select-digit{
+  background-color: lightblue;
+  padding: 10px;
+  border-radius: 50%;
+}
+</style>
