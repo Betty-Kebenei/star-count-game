141c63703db92d8bf6ab8a099711efcd67a91ff2
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-09-29 13:13:11 +0300

    Add levels: easy, medium and hard.

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,11 @@
       </div>
     </div>
     <div v-if="step === 1">
+      <button @click="chooseLevel('easy')">EASY</button>
+      <button @click="chooseLevel('medium')">MEDIUM</button>
+      <button @click="chooseLevel('hard')">HARD</button>
+    </div>
+    <div v-if="step === 2">
       <stop-watch />
       <h1>Challenge: {{ challenge }}</h1>
       <div id="star-section" v-for="star in stars">
@@ -20,8 +25,11 @@
         <button @click="submitAnswer">SUBMIT</button>
       </div>
       <div id="select-section">
-        <ul class="select-inner-section" v-for="num in 9">
-          <li class="select-digit" @click="generateAnswer(num)">{{num}}</li>
+        <ul class="select-inner-section" >
+          <li class="select-digit" @click="generateAnswer(0)">0</li>
+          <span v-for="num in 9">
+            <li class="select-digit" @click="generateAnswer(num)">{{num}}</li>
+          </span>
         </ul>
       </div>
     </div>
@@ -39,19 +47,31 @@ export default {
       answer: '',
       step: 0,
       answers: [],
-      challenge: 0
+      challenge: 0,
+      level: ''
     }
   },
   methods: {
     startGame() {
-      this.generateRandomNumber();
       this.step = 1;
+    },
+
+    chooseLevel(level) {
+      this.level = level;
+      this.generateRandomNumber(this.level);
+      this.step = 2;
       this.challenge = 1;
     },
 
-    generateRandomNumber() {
+    generateRandomNumber(level) {
       // Math.floor(Math.random() * (max -min +1)) + min;
-      this.stars = Math.floor(Math.random() * (9 - 1 +1) + 1 );
+      if (level === 'easy'){
+        this.stars = Math.floor(Math.random() * (9 - 1 +1) + 1 );
+      } else if (level === 'medium')  {
+        this.stars = Math.floor(Math.random() * (19 - 10 +1) + 10 );
+      } else if (level === 'hard') {
+        this.stars = Math.floor(Math.random() * (29 - 20 +1) + 20 );
+      } 
     },
 
     generateAnswer(num) {
@@ -64,11 +84,13 @@ export default {
  
     submitAnswer() {
       if(this.challenge !== 10) {
+        console.log(this.stars);
+        console.log(Number(this.answer));
         if(this.stars === Number(this.answer)) {
           this.answers.push(this.answer);
           this.challenge++;
           this.reset();
-          this.generateRandomNumber(); 
+          this.generateRandomNumber(this.level); 
         } else {
           alert('Wrong Answer, please submit another answer')
         }
